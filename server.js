4cf7eeed29293579f3ac066ce713f93466ba120a
Shamil <a_shoma@mail.ru>
2018-06-16 22:28:05 +0300

    сделал тестовый роут на получение артистов /artists

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const app = express();
 
+let artists = [
+    {
+        id: 1,
+        name: 'Metallice'
+    },
+    {
+        id: 2,
+        name: 'Iron Maiden'
+    },
+    {
+        id: 3,
+        name: 'Deep Perple'
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello API');
+});
+
+app.get('/artists', (req, res) => {
+    res.send(artists);
 });
 
 app.listen(5001, () => console.log('Listen port 5001'));
